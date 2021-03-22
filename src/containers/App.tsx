6104b997cc4fe76b3d707be3ef3a9f3613ff2304
Tan Wang Leng <tanwangleng@outlook.com>
2021-03-22 23:37:49 +0800

    Write list back to file

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,76 +3,91 @@ import "antd/dist/antd.css";
 import Title from "./Title";
 import ActionsBar from "./ActionsBar";
 import { open } from "tauri/api/dialog";
-import { readTextFile } from "tauri/api/fs";
+import { readTextFile, writeFile } from "tauri/api/fs";
 import TodoList from "./TodoList";
 
+interface TodoListFile {
+  fileLocation: string;
+  list: string[];
+}
+
 export default function App() {
-  const [fileLocation, setFileLocation] = React.useState<string>();
-  const [list, setList] = React.useState<string[]>([]);
+  const [file, setFile] = React.useState<TodoListFile>();
 
   const openFile = React.useCallback(async (): Promise<void> => {
     const newLocation = (await open({ filter: "todo" })) as string;
     const fileContent = (await readTextFile(newLocation)) as string;
-
-    setFileLocation(newLocation);
-    setList(fileContent.split("\n"));
+    setFile({ fileLocation: newLocation, list: fileContent.split("\n") });
   }, []);
 
+  React.useEffect((): void => {
+    // write back to file when list is updated
+    if (!file) return;
+    writeFile({ path: file.fileLocation, contents: file.list.join("\n") });
+  }, [file]);
+
   const addNewEntry = React.useCallback((): void => {
-    setList([...list, ""]);
-  }, [list]);
+    if (!file) return;
+    setFile({ ...file, list: [...file.list, ""] });
+  }, [file]);
 
   const updateEntry = React.useCallback(
     (updateIndex: number, newValue: string): void => {
-      setList(
-        list.map((entry, index) => (index === updateIndex ? newValue : entry))
-      );
+      if (!file) return;
+      setFile({
+        ...file,
+        list: file.list.map((entry, index) =>
+          index === updateIndex ? newValue : entry
+        ),
+      });
     },
-    [list]
+    [file]
   );
 
   const deleteEntry = React.useCallback(
     (deleteIndex: number): void => {
-      setList(list.filter((_, index) => index !== deleteIndex));
+      if (!file) return;
+      setFile({
+        ...file,
+        list: file.list.filter((_, index) => index !== deleteIndex),
+      });
     },
-    [list]
+    [file]
   );
 
   const moveEntryUp = React.useCallback(
     (moveIndex: number): void => {
-      if (moveIndex === 0) {
-        return;
-      }
+      if (!file) return;
+      if (moveIndex === 0) return;
 
-      const newList = [...list];
-      newList[moveIndex] = list[moveIndex - 1];
-      newList[moveIndex - 1] = list[moveIndex];
-      setList(newList);
+      const newList = [...file.list];
+      newList[moveIndex] = file.list[moveIndex - 1];
+      newList[moveIndex - 1] = file.list[moveIndex];
+      setFile({ ...file, list: newList });
     },
-    [list]
+    [file]
   );
 
   const moveEntryDown = React.useCallback(
     (moveIndex: number): void => {
-      if (moveIndex === list.length - 1) {
-        return;
-      }
+      if (!file) return;
+      if (moveIndex === file.list.length - 1) return;
 
-      const newList = [...list];
-      newList[moveIndex] = list[moveIndex + 1];
-      newList[moveIndex + 1] = list[moveIndex];
-      setList(newList);
+      const newList = [...file.list];
+      newList[moveIndex] = file.list[moveIndex + 1];
+      newList[moveIndex + 1] = file.list[moveIndex];
+      setFile({ ...file, list: newList });
     },
-    [list]
+    [file]
   );
 
   return (
     <>
       <Title />
-      <ActionsBar fileLocation={fileLocation} openFile={openFile} />
+      <ActionsBar fileLocation={file?.fileLocation} openFile={openFile} />
       <TodoList
-        list={list}
-        hasFileLoaded={!!fileLocation}
+        list={file?.list || []}
+        hasFileLoaded={!!file}
         addNewEntry={addNewEntry}
         updateEntry={updateEntry}
         deleteEntry={deleteEntry}
